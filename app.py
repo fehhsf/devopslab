89dec5fe3c344f06b6a14c05839da249123bc079
Felipe Ferreira <40368284+fehhsf@users.noreply.github.com>
2022-08-27 10:42:04 -0300

    Added DockerFile

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Alô Mundão"
+    return "Alô Mundão v2"
 
 if __name__ == '__main__':
-    app.run()
+    port = os.getenv('PORT')
+    app.run('0.0.0.0', port=port)
